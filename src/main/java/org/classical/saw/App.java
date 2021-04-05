755d0e47fd3f154156401aca0e464531ce8a7a25
Adnan Rahin <adnan.rahin13@gmail.com>
2021-04-04 20:03:02 -0400

    Classical SAW with Random Generator

--- src/main/java/org/classical/saw/App.java
+++ src/main/java/org/classical/saw/App.java
@@ -1,7 +1,36 @@
 package org.classical.saw;
 
+import org.classical.saw.randomwalks.SelfAvoidingRandomWalk;
+
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        long start = System.currentTimeMillis();
+
+        ExecutorService executorService = Executors.newFixedThreadPool(100000);
+
+        Future<Map<Integer, Double>> future = executorService.submit(new SelfAvoidingRandomWalk(1000000L));
+
+        Map<Integer, Double> result = future.get();
+
+        for (Integer key : result.keySet()) {
+            System.out.println(key + " -> " + result.get(key));
+        }
+
+        executorService.shutdown();
+
+        long end = System.currentTimeMillis();
+
+
+        double time = (end - start) / 1000.0;
+
+        System.out.println("Total Time to Execute " + time + " Seconds");
 
     }
 }
--- src/main/java/org/classical/saw/randomwalks/SelfAvoidingRandomWalk.java
+++ src/main/java/org/classical/saw/randomwalks/SelfAvoidingRandomWalk.java
@@ -1,20 +1,54 @@
 package org.classical.saw.randomwalks;
 
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Random;
+import java.util.Set;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentHashMap;
 
-public class SelfAvoidingRandomWalk implements Callable<Map<Integer, Integer>> {
+public class SelfAvoidingRandomWalk implements Callable<Map<Integer, Double>> {
 
-    private int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {1, 0}};
-    private Map<Integer, Integer> endToEndDistanceMap;
+    private final Map<Integer, Double> randomWalks;
+    private final int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    private final Random random = new Random();
+    private final Long limit;
 
-    public SelfAvoidingRandomWalk() {
-        this.endToEndDistanceMap = new ConcurrentHashMap<>();
+    public SelfAvoidingRandomWalk(Long limit) {
+        this.limit = limit;
+        this.randomWalks = new ConcurrentHashMap<>();
+    }
+
+    public void pseudoRandomWalkWith40Step() {
+
+        for (int i = 0; i < this.limit; i++) {
+            Set<String> isVisited = new HashSet<>();
+            int x = 0, y = 0;
+            for (int j = 1; j <= 40; j++) {
+                int index = random.nextInt(4);
+                x += this.directions[index][0];
+                y += this.directions[index][1];
+                if (!isVisited.contains(x + "," + y)) {
+                    isVisited.add(x + "," + y);
+                    double squareDistance = calculateDistanceBetweenPoints(0, 0, x, y);
+                    this.randomWalks.put(j, this.randomWalks.getOrDefault(j, 0.0)
+                            + squareDistance);
+                } else break;
+            }
+        }
     }
 
     @Override
-    public Map<Integer, Integer> call() throws Exception {
-        return this.endToEndDistanceMap;
+    public Map<Integer, Double> call() throws Exception {
+        pseudoRandomWalkWith40Step();
+        return this.randomWalks;
+    }
+
+    public double calculateDistanceBetweenPoints(
+            double x1,
+            double y1,
+            double x2,
+            double y2) {
+        return Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
     }
 }
